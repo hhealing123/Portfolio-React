cce321a8c13d4e2c05a256bb2940d498a8352628
Harvey Lee <hhealing123@gmail.com>
2022-12-04 19:11:01 -0600

    Revising Footer and deleting uncessary files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,4 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { MailchimpForm } from "./MailchimpForm";
-import logo from '../assets/img/headshot.jpg';
 import navIconHome from '../assets/img/nav-home-icon.svg';
 import navIconAbout from '../assets/img/nav-about-icon.svg';
 import navIconProjects from '../assets/img/nav-projects-icon.svg';
@@ -12,10 +10,7 @@ export const Footer = () => {
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          <MailchimpForm />
-          <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
-          </Col>
+          <Col size={12} sm={6} />
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="#"><img src={navIconHome} alt="Home Icon" /></a>
