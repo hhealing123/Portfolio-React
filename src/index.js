2fd57e8e8cd7b47c3111ec891e69b92151c3aa22
Harvey Lee <hhealing123@gmail.com>
2022-12-05 11:14:05 -0600

    Reverting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
-import { HashRouter as Router } from 'react-router-dom'
-
 ReactDOM.render(
-  <Router>
+  <React.StrictMode>
     <App />
-  </Router>, document.getElementById('root')
-  );
+  </React.StrictMode>,
+  document.getElementById('root')
+);
