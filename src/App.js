fa76bbc57d185d6b038a103a60a81c2f3eabcecb
Harvey Lee <hhealing123@gmail.com>
2022-12-04 18:42:48 -0600

    Adding contact page to render in app.js and adding contact icon

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Projects } from './components/Projects';
+import { Contact } from "./components/Contact";
 import { Footer } from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -11,6 +12,7 @@ function App() {
       <NavBar />
       <Banner />
       <Projects />
+      <Contact />
       <Footer />
     </div>
   );
